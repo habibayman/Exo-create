88a651bae8ddae5e36b04a4fbe1e3cc7a6263ca8
AhmedSobhy01 <samahmedsobhy@gmail.com>
2024-09-28 23:16:02 +0300

    chore: remove three.js package from project

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { templateCompilerOptions } from "@tresjs/core";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      ...templateCompilerOptions,
-    }),
-  ],
+  plugins: [vue()],
 });
