12b78449ed5b8f2967b2ff26c70bd691d17f6c48
AhmedSobhy01 <samahmedsobhy@gmail.com>
2024-10-05 00:40:06 +0300

    refactor(quiz): update quiz questions and options

--- src/data/quizQuestions.js
+++ src/data/quizQuestions.js
@@ -2,49 +2,124 @@ export default [
   {
     text: 'What is an exoplanet?',
     options: [
-      { text: 'A planet outside of our solar system', correct: true },
-      { text: 'A planet with exoskeleton', correct: false },
-      { text: 'A planet that is extremely large', correct: false },
+      { text: 'A planet within our solar system.', correct: false },
+      { text: 'A planet that orbits our sun.', correct: false },
       {
-        text: 'A planet that exists within our solar system',
+        text: 'A planet that orbits a star outside our solar system.',
+        correct: true,
+      },
+      { text: 'A planet with no stars around it.', correct: false },
+    ],
+  },
+  {
+    text: 'What were the first exoplanet types found?',
+    options: [
+      { text: 'Terrestrial', correct: false },
+      { text: 'Super-Earth', correct: false },
+      { text: 'Hot Jupiters', correct: true },
+      { text: 'Neptunian', correct: false },
+    ],
+  },
+  {
+    text: 'What happens when a planet transits in front of its star?',
+    options: [
+      { text: "The planet's atmosphere becomes visible.", correct: false },
+      { text: 'The star temporarily becomes brighter.', correct: false },
+      { text: 'The star temporarily becomes dimmer.', correct: true },
+      { text: 'The planet begins to wobble.', correct: false },
+    ],
+  },
+  {
+    text: 'Super-Earth exoplanets are more massive than what and lighter than what?',
+    options: [
+      { text: 'Saturn - Earth', correct: false },
+      { text: 'Neptune - Saturn', correct: false },
+      { text: 'Earth - Neptune', correct: true },
+      { text: 'Mercury - Earth', correct: false },
+    ],
+  },
+  {
+    text: "When a planet passes directly between an observer and the star it orbits, it blocks some of that star's light. This is what method?",
+    options: [
+      { text: 'Transit', correct: true },
+      { text: 'Gravitational Microlensing', correct: false },
+      { text: 'Direct imaging', correct: false },
+      { text: 'Radial velocity', correct: false },
+    ],
+  },
+  {
+    text: 'What is a habitable zone?',
+    options: [
+      {
+        text: 'The distance from a star where a planet is likely to have liquid water.',
+        correct: true,
+      },
+      { text: 'A region of space with no stars.', correct: false },
+      { text: 'An area where planets cannot exist.', correct: false },
+      { text: 'A zone where rogue planets are found.', correct: false },
+    ],
+  },
+  {
+    text: 'What are the two main techniques to find exoplanets?',
+    options: [
+      { text: 'Transit & Gravitational Microlensing', correct: false },
+      { text: 'Astrometry & Direct imaging', correct: false },
+      { text: 'Transit & Direct imaging', correct: false },
+      { text: 'Radial velocity & Transit', correct: true },
+    ],
+  },
+  {
+    text: "In the Radial velocity method, if the star moves in the direction of the observer, it will appear to be shifted toward what in the star's light spectrum?",
+    options: [
+      { text: 'Red', correct: false },
+      { text: 'Blue', correct: true },
+      { text: 'Green', correct: false },
+      { text: 'Yellow', correct: false },
+    ],
+  },
+  {
+    text: 'What is the ultimate goal of searching for exoplanets?',
+    options: [
+      {
+        text: 'Learning more about the formation and evolution of planetary systems',
         correct: false,
       },
+      {
+        text: 'Finding unmistakable signs of current life',
+        correct: false,
+      },
+      {
+        text: 'Understanding the history and future of Earth',
+        correct: false,
+      },
+      { text: 'All of the above', correct: true },
     ],
   },
   {
-    text: 'Which of these methods is used to discover exoplanets?',
+    text: 'What happens when a planet transits in front of its star?',
     options: [
-      { text: 'Telescope', correct: false },
-      { text: 'Echo-location', correct: false },
-      { text: 'Seismic Waves', correct: false },
-      { text: 'Infrared Imaging', correct: true },
+      { text: "The planet's atmosphere becomes visible.", correct: false },
+      { text: 'The star temporarily becomes brighter.', correct: false },
+      { text: 'The star temporarily becomes dimmer.', correct: true },
+      { text: 'The planet begins to wobble.', correct: false },
     ],
   },
   {
-    text: 'What is the name of the first exoplanet discovered?',
+    text: 'Which planet of these has 2 stars?',
     options: [
-      { text: 'Kepler-186f', correct: false },
-      { text: 'Proxima Centauri b', correct: false },
-      { text: '51 Pegasi b', correct: true },
-      { text: 'Gliese 581g', correct: false },
+      { text: 'KOI-55b', correct: false },
+      { text: 'GJ 1132 b', correct: false },
+      { text: 'HD 189733 b', correct: false },
+      { text: 'Kepler-16b', correct: true },
     ],
   },
   {
-    text: 'How many exoplanets have been discovered so far?',
+    text: 'How can we learn about the atmosphere of distant planets?',
     options: [
-      { text: 'Over 1000', correct: false },
-      { text: 'Over 5000', correct: true },
-      { text: 'Over 10,000', correct: false },
-      { text: 'Over 100,000', correct: false },
-    ],
-  },
-  {
-    text: 'What is the name of the exoplanet that is most similar to Earth?',
-    options: [
-      { text: 'Kepler-186f', correct: true },
-      { text: 'Proxima Centauri b', correct: false },
-      { text: '51 Pegasi b', correct: false },
-      { text: 'Gliese 581g', correct: false },
+      { text: 'Direct imaging', correct: false },
+      { text: 'Analyzing starlight', correct: true },
+      { text: 'Searching for shadows', correct: false },
+      { text: 'Measuring the planet’s wobble', correct: false },
     ],
   },
 ];
